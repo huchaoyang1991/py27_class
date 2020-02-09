294fcc96c97b9a6fc37bc8bf183fe72494f94d6e
Sun <sun@163.com>
2020-02-09 21:08:56 +0800

    update task_03day

--- py27_03day/task_03day.py
+++ py27_03day/task_03day.py
@@ -1,4 +1,5 @@
 import random
+
 # 1、现在有变量 a = ‘hello’ ,    b = ‘python18’     c = ‘!’  ,通过相关操作转换成字符串：'hello python18 !'（注意点:转换之后单词之间有空格）
 
 
@@ -9,8 +10,13 @@ c = '!'
 print(s_blank.join((a, b, c)))
 
 # 2、使用random模块和字符串拼接的方法，随机生成一个130开头的手机号码（只能使用上课学过的知识去做）。
-num = random.randint(10000000, 99999999)
-print('130' + str(num))
+phone = '130'
+for i in range(8):
+    num = str(random.randint(0, 9))
+    phone += num
+# num = random.randint(10000000, 99999999)
+# print('130' + str(num))
+print(phone)
 
 # 3、有一个如下列表，请编写代码，提示用户输入1-7中的数字，分别代表周一到周日，根据用户输入，打印输出“今天是周X”（要求：使用上课学过的知识点来做）
 

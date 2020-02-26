b0c3f88550579fe242b198fb18d83732d2ffd0ec
Sun <sun@163.com>
2020-02-26 23:41:04 +0800

    add task_10day

---
+++ py27_10day/03查看模块导入的搜索路径.py
@@ -0,0 +1,14 @@
+"""
+============================
+Author:柠檬班-木森
+Time:2020/2/25   21:22
+E-mail:3247119728@qq.com
+Company:湖南零檬信息技术有限公司
+============================
+"""
+import sys
+
+# print(sys.path)
+
+for i in sys.path:
+    print(i)
